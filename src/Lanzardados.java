637c678a0c1b6c792400b07a811a29b6c2720366
sofiagiraldo <nsgiraldog@ucundinamarca.edu.co>
2025-09-11 00:15:18 -0500

    ejercicios

--- src/Lanzardados.java
+++ src/Lanzardados.java
@@ -3,26 +3,63 @@ import java.util.Scanner;
 
 public class Lanzardados {
     public static void main(String[] args) {
+        menu();
     }
     public static void menu() {
         Scanner sc = new Scanner(System.in);
         int opcion=0;
-        while(opcion!=3){
+        while(opcion!=6){
 
-            System.out.print("Menu: ");
-            System.out.println("1.Ejercicio dados 2 ");
-            System.out.println("2.Ejercicio dados 3 ");
-            System.out.println("3.IMC");
-            System.out.println("4.");
-            System.out.println("5.");
-            System.out.println("6.Salir");
+            System.out.print("\n Menú principal: ");
 
+            System.out.println("\n 1.Ejercicio dados 2 ");
+            System.out.println("\n 2.Ejercicio dados 3 ");
+            System.out.println("\n 3.Calcular IMC");
+            System.out.println("\n 4.Ejercicio estudiantes");
+            System.out.println("\n 5.");
+            System.out.println("\n 6.Salir");
+            System.out.println("\n Elige una opción");
+            opcion = sc.nextInt();
+            switch (opcion){
+                case 1 -> dados2();
+                case 2 -> dados3();
+                case 3 -> IMC();
+                case 4 -> ejercicio4();
+                case 5 -> System.out.println("Ejercicio 5 aún no implementado.");
+                case 6 -> System.out.println("Saliendo del programa...");
+                default -> System.out.println("Opción inválida. Intente de nuevo.");
+            }
         }
     }
 
-    public static void dados2(){
 
+    public static void dados2(){
+        Random rand = new Random();
+
+        int dado1 = 0, dado2 = 0;
+
+        for (int i = 1; i <= 20; i++) {
+            int tiro1 = rand.nextInt(6) + 1;
+            int tiro2 = rand.nextInt(6) + 1;
+            dado1 += tiro1;
+            dado2 += tiro2;
+            System.out.println("Tiro " + i + ": Dado1=" + tiro1 + " | Dado2=" + tiro2);
+        }
+        System.out.println("Resultados finales");
+        System.out.println("Dado 1: " + dado1);
+        System.out.println("Dado 2: " + dado2);
+
+        if (dado1 > dado2) {
+            System.out.println("Ganador: Dado 1 ");
+            System.out.println("Perdedor: Dado 2 ");
+        } else if (dado2 > dado1) {
+            System.out.println("Ganador: Dado 2 ");
+            System.out.println("Perdedor: Dado 1 ");
+        } else {
+            System.out.println("Empate entre los dos dados ");
+        }
     }
+
     public static void dados3(){
 
         Random aleatorio = new Random();
@@ -70,40 +107,92 @@ public class Lanzardados {
                     nombreMitad = "dado3";
                 }
 
-            System.out.println("Mejor:" + nombreMejor + "con" + mejor);
-            System.out.println("Mitad:" + nombreMitad + "con" + mitad);
-            System.out.println("Peor:" + nombrePeor + "con" + peor);
+            System.out.println("Mejor:" + nombreMejor + "\ncon" + mejor);
+            System.out.println("Mitad:" + nombreMitad + "\ncon" + mitad);
+            System.out.println("Peor:" + nombrePeor + "\ncon" + peor);
             }
 
             public static String IMC() {
                 Scanner sc = new Scanner(System.in);
                 System.out.println("Ingresa tu peso en kg: ");
                 double peso = sc.nextDouble();
-                System.out.println("Ingresa tu estatura");
+                System.out.println("Ingresa tu estatura en metros");
                 double estatura = sc.nextDouble();
                 double imc = peso/ Math.pow(estatura, 2);
                 String situacion;
                 if (imc < 18.5) {
                     situacion = "Bajo peso";
-                } else if (imc < 24.9) {
+                } else if (imc >= 18.5 && imc <= 24.9) {
                     situacion = "Peso normal";
-                } else if (imc < 26.9) {
+                } else if (imc >= 25 && imc <= 26.9) {
                     situacion = "Sobrepeso grado 1";
-                } else if (imc < 29.9) {
+                } else if (imc >= 27 && imc <= 29.9) {
                     situacion = "Sobrepeso grado 2";
-                } else if (imc < 34.9) {
+                } else if (imc >= 30 && imc <= 34.9) {
                     situacion = "Obesidad tipo 1";
-                } else if (imc < 39.9) {
+                } else if (imc >= 35 && imc <= 39.9) {
                     situacion = "Obesidad tipo 2";
-                } else if (imc < 49.9) {
+                } else if (imc >= 40 && imc <= 49.9) {
                     situacion = "Obesidad tipo 3 (mórbida)";
                 } else {
-                    situacion = "Obesidad tipo 4 (extremo)";
+                    situacion = "Obesidad tipo 4 (extrema)";
                 }
 
                 System.out.printf("Tu IMC es: %.2f\n", imc);
                 System.out.println("Situación: " + situacion);
                 return situacion;
             }
+
+public static void ejercicio4(){
+    Scanner sc = new Scanner(System.in);
+    String repetir;
+    do {
+        System.out.println("Ingresa la cantidad de estudiantes");
+        int cantidad = sc.nextInt();
+
+        String[] nombres = new String[cantidad];
+        double[] promedios = new double[cantidad];
+
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("Ingresa el nombre del estudiante " + (i + 1) + ": ");
+            nombres[i] = sc.next();
+
+            double suma = 0;
+            for (int j = 1; j <= 3; j++) {
+                System.out.println("Ingrese la nota " + j + " de " + nombres[i] + ": ");
+                double nota = sc.nextDouble();
+                suma += nota;
+            }
+
+            promedios[i] = suma / 3;
+        }
+
+        System.out.println("Resultados Finales");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("Estudiante: " + nombres[i]);
+            System.out.println("Promedio: " + promedios[i]);
+
+            int rango = (int) promedios[i];
+            switch (rango) {
+                case 0, 1, 2:
+                    System.out.println("Estado: Reprobado");
+                    break;
+                case 3:
+                    System.out.println("Estado: Aprobado");
+                    break;
+                case 4, 5:
+                    System.out.println("Estado: Excelente");
+                    break;
+                default:
+                    System.out.println("Nota inválida");
+            }
+            System.out.println("----------------------");
+        }
+        System.out.println("¿Desea registrar otro grupo de estudiantes? (si/no): ");
+        repetir = sc.next();
+    } while (repetir.equalsIgnoreCase("si"));
+
+    System.out.println("Programa finalizado.");
+    }
 }
 
